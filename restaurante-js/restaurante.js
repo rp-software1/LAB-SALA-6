5f2a65bfa903170c8b7b012303c723d3c90651b6
luis890je <luisxdghklkhv9@gmail.com>
2026-08-05 10:01:33 -0500

    Dia1-A: [Hemos llegado a uzar las variables let y const para poder revisar si el nombre del plato y el precio de plato varian o son fijos y llegando a revisar si son correctos.]

---
+++ restaurante-js/restaurante.js
@@ -0,0 +1,21 @@
+// restaurante.js — versión Bloque A
+ 
+// Plato 1
+const nombrePlato = "Lomo saltado";
+const precioPlato = 18;
+let stockPlato = 5;
+ 
+// Plato 2
+const nombrePlato2 = "Arroz con leche";
+const precioPlato2 = 8;
+let stockPlato2 = 10;
+ 
+// Plato 3
+const nombrePlato3 = "Sopa criolla";
+const precioPlato3 = 12;
+let stockPlato3 = 3;
+ 
+console.log("Plato 1:", nombrePlato, "| Precio:", precioPlato, "| Stock:", stockPlato);
+console.log("Plato 2:", nombrePlato2, "| Precio:", precioPlato2, "| Stock:", stockPlato2);
+console.log("Plato 3:", nombrePlato3, "| Precio:", precioPlato3, "| Stock:", stockPlato3);
+
