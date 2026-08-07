d20055b33eab2881dd3d1d85195d5fce37455549
silvajararenzo-coder <silvajararenzo@gmail.com>
2026-08-07 10:24:52 -0500

    Agrega función contarPlatos

--- restaurante-js/app.js
+++ restaurante-js/app.js
@@ -3,8 +3,8 @@ let menu = [
  { nombre: "Arroz con pollo", precio: 12, stock: 5 },
  { nombre: "Lomo saltado", precio: 18, stock: 3 },
  { nombre: "Sopa", precio: 8, stock: 10 },
- { nombre: "Carapulcra", precio: 11, stock: 7 },
- { nombre: "Ceviche", precio: 14, stock: 4 }
+ { nombre: "Chaufa", precio: 10, stock: 2 },
+ { nombre: "Ceviche", precio: 15, stock: 7 }
 ];
 
 // 2) FUNCIÓN: renderizar (mostrar) el menú en pantalla
@@ -17,16 +17,17 @@ function renderMenu() {
 
  for (let i = 0; i < menu.length; i++) {
    const plato = menu[i];
-   html += `<li>${plato.nombre} — S/ ${plato.precio} — Stock: ${plato.stock}</li>`;
+   html += <li>${plato.nombre} — S/ ${plato.precio} — Stock: ${plato.stock}</li>;
  }
 
- html += "</ul>";
- output.innerHTML = html;
+ html += <p>Total de platos en el menú: ${contarPlatos()}</p>;
+
+    output.innerHTML = html;
 }
 
 // 3) FUNCIÓN: agregar un plato demo al menú
 function agregarPlatoDemo() {
- const nuevoPlato = { nombre: "Pollo a la brasa", precio: 20, stock: 4 };
+ const nuevoPlato = { nombre: "Arroz tapado", precio: 13, stock: 9 };
  menu.push(nuevoPlato);
 }
 
@@ -39,3 +40,7 @@ document.getElementById("btnAgregar").addEventListener("click", () => {
  agregarPlatoDemo();
  renderMenu();
 });
+
+function contarPlatos() {
+    return menu.length;
+}
