c5e496242f53bf5887c88fe83045b0b0a2586483
silvajararenzo-coder <silvajararenzo@gmail.com>
2026-08-07 09:59:11 -0500

    Agrega platos adicionales al menú y combio de nombre del restaurante

--- restaurante-js/app.js
+++ restaurante-js/app.js
@@ -2,7 +2,9 @@
 let menu = [
  { nombre: "Arroz con pollo", precio: 12, stock: 5 },
  { nombre: "Lomo saltado", precio: 18, stock: 3 },
- { nombre: "Sopa", precio: 8, stock: 10 }
+ { nombre: "Sopa", precio: 8, stock: 10 },
+ { nombre: "Carapulcra", precio: 11, stock: 7 },
+ { nombre: "Ceviche", precio: 14, stock: 4 }
 ];
 
 // 2) FUNCIÓN: renderizar (mostrar) el menú en pantalla
