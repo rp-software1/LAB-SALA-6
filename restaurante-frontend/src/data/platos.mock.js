9cb320a4ac772d6c2f898a3ee70c627f384d5b23
luis890je <luisxdghklkhv9@gmail.com>
2026-08-18 08:19:19 -0500

    Bloque A — proyecto Vite en StackBlitz creado y verificado

--- restaurante-frontend/src/data/platos.mock.js
+++ restaurante-frontend/src/data/platos.mock.js
@@ -1,43 +1,43 @@
-// Forma exacta del contrato API con Node
+// src/data/platos.mock.js
 export const platosMock = [
-    {
-        id: 1,
-        nombre: "Lomo saltado",
-        categoria: "segundos",
-        precio: 18,
-        stock: 3,
-        disponible: true
-    },
-    {
-        id: 2,
-        nombre: "Arroz con pollo",
-        categoria: "segundos",
-        precio: 12,
-        stock: 5,
-        disponible: true
-    },
-    {
-        id: 3,
-        nombre: "Sopa de verduras",
-        categoria: "entradas",
-        precio: 8,
-        stock: 0,
-        disponible: false
-    },
-    {
-        id: 4,
-        nombre: "Pollo a la brasa",
-        categoria: "segundos",
-        precio: 20,
-        stock: 4,
-        disponible: true
-    },
-    {
-        id: 5,
-        nombre: "Jugo de naranja",
-        categoria: "bebidas",
-        precio: 5,
-        stock: 8,
-        disponible: true
-    }
+  {
+    id: 1,
+    nombre: "Lomo saltado",
+    categoria: "segundos",
+    precio: 18,
+    stock: 3,
+    disponible: true
+  },
+  {
+    id: 2,
+    nombre: "Arroz con pollo",
+    categoria: "segundos",
+    precio: 12,
+    stock: 5,
+    disponible: true
+  },
+  {
+    id: 3,
+    nombre: "Sopa de verduras",
+    categoria: "entradas",
+    precio: 8,
+    stock: 0,
+    disponible: false
+  },
+  {
+    id: 4,
+    nombre: "Pollo a la brasa",
+    categoria: "segundos",
+    precio: 20,
+    stock: 4,
+    disponible: true
+  },
+  {
+    id: 5,
+    nombre: "Jugo de naranja",
+    categoria: "bebidas",
+    precio: 5,
+    stock: 8,
+    disponible: true
+  }
 ];
