6572bf3f8e315df3f8e11b41cb99082f9934aeab
luis890je <luisxdghklkhv9@gmail.com>
2026-09-22 10:46:07 -0500

    PRESAID-D5-BLOQUE-A: Preparacion de PostgreSQL y variables de entorno

--- restaurante-presaid/src/app.module.ts
+++ restaurante-presaid/src/app.module.ts
@@ -6,14 +6,26 @@ import { PedidosModule } from './pedidos/pedidos.module';
 import { ComandasModule } from './comandas/comandas.module';
 import { TicketsModule } from './tickets/tickets.module';
 
+const isProduction = !!process.env.DATABASE_URL;
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'better-sqlite3',
-      database: 'db.sqlite',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(
+      isProduction
+        ? {
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true,
+            ssl: { rejectUnauthorized: false },
+          }
+        : {
+            type: 'better-sqlite3',
+            database: 'db.sqlite',
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true,
+          },
+    ),
     PlatosModule,
     MesasModule,
     PedidosModule,
